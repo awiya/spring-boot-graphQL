58b65ff7f05ecc23ce6d07cd800112b994795b25
AYOUB <a.idihia@gmail.com>
2023-04-11 13:39:45 +0200

    add the configuration of a MySQL database in the application.yml file, and also change the file extension from properties to yml. adding some data to the database via the CommandLineRunner (hard coding data) for testing purposes

--- src/main/java/com/idihia/springgraphQL/SpringGraphQlApplication.java
+++ src/main/java/com/idihia/springgraphQL/SpringGraphQlApplication.java
@@ -1,13 +1,52 @@
 package com.idihia.springgraphQL;
 
+
+import com.idihia.springgraphQL.entities.Category;
+import com.idihia.springgraphQL.entities.Product;
+import com.idihia.springgraphQL.repository.CategoryRepository;
+import com.idihia.springgraphQL.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 
 @SpringBootApplication
 public class SpringGraphQlApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringGraphQlApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGraphQlApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(CategoryRepository categoryRepository,
+                                        ProductRepository productRepository) {
+        return args -> {
+            Random random = new Random();
+
+            List.of("Computer", "Printer", "Smartphone").forEach(cat -> {
+                Category category = Category.builder()
+                        .name(cat)
+                        .build();
+                categoryRepository.save(category);
+            });
+
+            categoryRepository.findAll().forEach(category -> {
+                for (int i = 1; i <= 10; i++) {
+                    Product product = Product.builder()
+                            .id(UUID.randomUUID().toString())
+                            .name(category.getName() + " " + i)
+                            .price(100 + random.nextDouble() * (50000 - 100))
+                            .quantity(1 + random.nextInt(99))
+                            .category(category)
+                            .build();
+                    productRepository.save(product);
+                }
+            });
+        };
+    }
 
 }
--- src/main/java/com/idihia/springgraphQL/repository/CategoryRepository.java
+++ src/main/java/com/idihia/springgraphQL/repository/CategoryRepository.java
@@ -4,4 +4,5 @@ import com.idihia.springgraphQL.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findByName(String name);
 }
